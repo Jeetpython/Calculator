cf605c7021a32340921709552fdde3ee83abca2f
Jeet <jhvram2013@gmail.com>
2024-05-24 12:56:21 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+operation = input("Which operation?")
+number1  = int(input("Number One?"))
+number2 = int(input("Number two?"))
+
+if operation == "+" :
+    print(number1 + number2)
+    
+if operation == "-" :
+    print(number1 - number2)
+
+if operation == "*" :
+    print(number1 * number2)
+
+if operation == "/" :
+    print(number1 / number2)
